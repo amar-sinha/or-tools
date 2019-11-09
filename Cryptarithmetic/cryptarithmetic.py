34a5ccae7177732cdc54a9b3a5c36089ddc205ad
amar-sinha <amar.sinha@uconn.edu>
2019-11-09 00:54:56 -0500

    generate word, split word, and letter arrays

--- Cryptarithmetic/cryptarithmetic.py
+++ Cryptarithmetic/cryptarithmetic.py
@@ -18,8 +18,38 @@ class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
     def solution_count(self):
         return self.__solution_count
 
+def split_string(str):
+    return [char for char in str]
+
 def cryptarithmetic():
-    pass
+    # Constraint programming engine
+    model = cp_model.CpModel()
+
+    print(
+        """Enter any number of words by entering each
+word on a newline. For n words, all words up to
+the (n-1)th word will sum to equal the nth word.""")
+
+    word_array = []
+    split_word_array = []
+    letters = []
+    
+    while True:
+        word = input()
+        if word:
+            word_array.append(word)
+            split_word = split_string(word)
+            split_word_array.append(split_word)
+            
+            for char in split_word:
+                if char not in letters:
+                    letters.append(char)
+        else:
+            break
+    
+    print(word_array)
+    print(letters)
+    print(split_word_array)
 
 if __name__ == "__main__":
     cryptarithmetic()
