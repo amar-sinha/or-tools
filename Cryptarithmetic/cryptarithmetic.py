c7a1e9b9cb9270f26cfcfd4ec359d0cae46468b6
amar-sinha <amar.sinha@uconn.edu>
2019-11-09 00:34:05 -0500

    add Cryptarithmetic puzzle

---
+++ Cryptarithmetic/cryptarithmetic.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from ortools.sat.python import cp_model
+
+class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
+    ## Print intermediate solutions
+
+    def __init__(self, variables):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.__variables = variables
+        self.__solution_count = 0
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+        for v in self.__variables:
+            print('%s=%i' % (v, self.Value(v)), end=' ')
+        print()
+
+    def solution_count(self):
+        return self.__solution_count
+
+def cryptarithmetic():
+    pass
+
+if __name__ == "__main__":
+    cryptarithmetic()
